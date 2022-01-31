96d90029fc4088e752cb95b5bc8d0bcb8eca8539
shwetaps605 <shwetaroy305@gmail.com>
2022-01-31 13:19:16 +0530

    First commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker'
+
+  constructor() { }
+
+  //Lifecycle method like componentDidMount
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    console.log("Button clickedng  !")
+  }
+
+}
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core'
+import {TASKS} from '../../mock-tasks'
+import { Task } from '../../Task'
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
